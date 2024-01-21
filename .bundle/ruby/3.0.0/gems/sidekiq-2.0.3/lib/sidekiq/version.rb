c13c89525b0e2806f0d84631b38510104f265321
Emerson Almeida <emersonalmeidax@gmail.com>
2024-01-21 13:41:28 -0300

    update it

--- .bundle/ruby/3.0.0/gems/sidekiq-2.0.3/lib/sidekiq/version.rb
+++
@@ -1,3 +0,0 @@
-module Sidekiq
-  VERSION = "2.0.3"
-end
--- .bundle/ruby/3.0.0/gems/sidekiq-2.0.3/myapp/config/initializers/sidekiq.rb
+++
@@ -1,6 +0,0 @@
-Sidekiq.configure_client do |config|
-  config.redis = { :size => 2 }
-end
-Sidekiq.configure_server do |config|
-  config.redis = { :size => 25 }
-end
